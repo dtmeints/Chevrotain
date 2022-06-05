b0199ec34e85a542474efa63b479bf3f990f8544
dtmeints <100872785+dtmeints@users.noreply.github.com>
2022-06-05 13:47:57 -0500

    Add files via upload

--- Inventory.cs
+++ Inventory.cs
@@ -2,83 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using ItemTypes;
 using InventoryManagement;
+using Quills;
 
 public static class Inventory
 {
+    public static Dictionary<ItemType, (int current, int max)> ActiveInventory = new Dictionary<ItemType, (int current, int max)>();
+    public static Dictionary<QuillType, bool> Quills = new Dictionary<QuillType, bool>();
 
     public const int saltJarCapacity = 8;
     public const int maxSaltJars = 12;
-    
+
     //private JarContainer _jarContainer;
 
-      
-    public static event Action<int> OnSaltJarChange;
-    public static event Action<int> OnSaltChange;
-    public static event Action<int> OnBarbChange;
+
     public static event Action OnInitializeHUD;
 
-    public static void AddItem(item type, int amount = 1)
-    {
-        switch (type)
-        {
-            case item.salt:
-                AddSalt(amount);
-                break;
-            case item.saltJar:
-                AddSaltJar(amount);
-                break;
-            case item.barb:
-               AddBarb(amount);
-                break;
-        }    
-    }
+    public static event Action<ItemType, int> OnInventoryChange;
 
-    public static void AddSalt(int amount)
+    public static void AddToInventory(ItemType itemType, int amount = 1)
     {
-        if (ActiveData.inventory[item.salt].current + amount > ActiveData.inventory[item.salt].max)
+        var itemsInInventory = ActiveInventory[itemType];
+        if (itemsInInventory.current + amount > itemsInInventory.max)
         {
             amount = 0;
         }
-        OnSaltChange?.Invoke(amount);
+
+        OnInventoryChange?.Invoke(itemType, amount);
     }
 
-
-    public static void AddBarb(int amount)
+    public static void LoadInventory(ItemType itemType, int amount)
     {
-        ActiveData.inventory[item.barb] = (ActiveData.inventory[item.barb].current + amount, ActiveData.inventory[item.barb].max);
-    }
+        var itemToLoad = ActiveInventory[itemType];
 
-    private static void AddSaltJar(int amount)
-    {
-        if (ActiveData.inventory[item.saltJar].current + amount > ActiveData.inventory[item.saltJar].max)
+        switch (itemType)
         {
-            amount = 0;
+            case ItemType.salt:
+                itemToLoad = (itemToLoad.current += amount, itemToLoad.max);
+                OnInitializeHUD?.Invoke();
+                break;
+            case ItemType.saltJar:
+                itemToLoad = (amount, maxSaltJars);
+                break;
+            case ItemType.barb:
+                itemToLoad = (amount, itemToLoad.max);
+                break;
         }
-        OnSaltChange?.Invoke(amount);
-    }
-
-    public static void LoadSalt(int amount)
-    {
-        ActiveData.inventory[item.salt] = (ActiveData.inventory[item.salt].current + amount, ActiveData.inventory[item.salt].max);
-        OnInitializeHUD?.Invoke();
-    }
-
-    public static void LoadSaltJars(int amount)
-    {
-        ActiveData.inventory[item.saltJar] = (amount, maxSaltJars);
-    }
-
-    public static void LoadCurrentBarbs(int amount)
-    {
-        ActiveData.inventory[item.barb] = (amount, ActiveData.inventory[item.barb].max);
     }
 }
 
 namespace InventoryManagement
 {
-    public enum item
+    public enum ItemType
     {
         salt,
         saltJar,
